3bbb614e230bd2bfce41caecb356aab46480ea1c
Rodrigo Bossini <professorbossini@gmail.com>
2023-10-16 11:02:43 -0300

    ap03(auth): pagina 37
    - retomar falando de jwt

--- autenticacao_autorizacao_app/serializers/user_serializer.py
+++ autenticacao_autorizacao_app/serializers/user_serializer.py
@@ -1,8 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+import re
 
 class UserSerializer(serializers.ModelSerializer):
   password = serializers.CharField(write_only=True)
+
+  def validate_password(self, password):
+    #se tem maiúscula
+    if not re.search('[A-Z]', password):
+      raise serializers.ValidationError("Pelo menos uma maiúscula")
+    #se tem minúscula
+    if not re.search('[a-z]', password):
+      raise serializers.ValidationError("Pelo menos uma minúscula")
+    if not re.search('[0-9]', password):
+      raise serializers.ValidationError("Pelo menos um número")
+    if not re.search('[^A-Za-z0-9]', password):
+      raise serializers.ValidationError("Pelo menos um símbolo especial")
+    if len(password) < 8:
+      raise serializers.ValidationError("Comprimento pelo menos 8")
+    return password
+
   class Meta:
     model = User
     fields = ['id', 'username', 'email', 'password']
--- autenticacao_autorizacao_app/views/usuario_views.py
+++ autenticacao_autorizacao_app/views/usuario_views.py
@@ -1,4 +1,17 @@
-from rest_framework import views
+from rest_framework import views, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from autenticacao_autorizacao_app.serializers import UserSerializer
 class CadastroNovoUsuarioView(views.APIView):
-  #pesquisar diferença entre pass e ...
-  ...
+  def post(self, request):
+    serializer = UserSerializer (data=request.data)
+    if serializer.is_valid():
+      #keyword args
+      #dados = {name: "Rodrigo", "age": 18}
+      #keyword args
+      #def seila(**kwargs):
+      #  pass
+      #lista =[1, 2, 3]
+      user = User.objects.create_user(**serializer.validated_data)
+      return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
